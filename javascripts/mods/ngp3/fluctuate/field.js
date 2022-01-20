774eccf8a4dd05c32629d667020a947692b59fa3
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2022-01-19 23:02:08 -0500

    1/19/22a

--- javascripts/mods/ngp3/fluctuate/field.js
+++ javascripts/mods/ngp3/fluctuate/field.js
@@ -3,10 +3,37 @@ let ff = {
 	unl: (force) => force ? fluc.unl() : ff_tmp.unl && ff_tmp.eff,
 
 	data: {
+		all: ["alt", "pow", "free", "syn", "upg", "share"],
+		alt: {
+			req: 1,
+			desc: "Increases the altitudes."
+		},
+		pow: {
+			req: 2,
+			desc: "Multiplies the power gain."
+		},
+		free: {
+			req: 3,
+			desc: "Decreases the power requirement."
+		},
+		syn: {
+			req: 6,
+			desc: "Strengthens Strings by altitudes."
+		},
+		upg: {
+			req: 7,
+			desc: "Unlocks a new power."
+		},
+		share: {
+			req: 10,
+			desc: "Shares altitudes to the right."
+		}
 	},
 
 	setup() {
 		ff_save = {
+			arcs: {}, //{1: [1, 4], 2: [1, 3]}
+			perks: {} //{1: "alt", 2: "pow"}
 		}
 		fluc_save.ff = ff_save
 		return ff_save
