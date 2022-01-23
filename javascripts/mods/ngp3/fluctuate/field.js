a4f44dfaa364c40a131c3956ad313a6a3f630d4b
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2022-01-22 23:09:00 -0500

    1/22/22a

--- javascripts/mods/ngp3/fluctuate/field.js
+++ javascripts/mods/ngp3/fluctuate/field.js
@@ -3,7 +3,8 @@ let ff = {
 	unl: (force) => force ? fluc.unl() : ff_tmp.unl && ff_tmp.eff,
 
 	data: {
-		all: ["alt", "pow", "free", "syn", "upg", "share"],
+		all: [null, "alt", "pow", "free", "syn", "upg", "share"],
+		modes: ["Arc", "Remove"],
 		reqs: [0, 1, 2, 4, 6, 8, 10],
 		alt: {
 			req: 1,
@@ -40,7 +41,8 @@ let ff = {
 	setup() {
 		ff_save = {
 			arcs: {}, //{1: [1, 4], 2: [1, 3]}
-			perks: {} //{1: "alt", 2: "pow"}
+			perks: {}, //{1: "alt", 2: "pow"}
+			mode: 0
 		}
 		fluc_save.ff = ff_save
 		return ff_save
@@ -50,13 +52,16 @@ let ff = {
 		if (!tmp.ngp3) return
 
 		var data = ff_save || this.setup()
+		if (!data.mode) data.mode = 0
 
+		this.switchMode(true)
 		this.updateTmp()
 	},
 	reset() {
 		ff_save = {
 			arcs: {}, //{1: [1, 4], 2: [1, 3]}
-			perks: {} //{1: "alt", 2: "pow"}
+			perks: {}, //{1: "alt", 2: "pow"}
+			mode: 0
 		}
 		ff.updateTmp()
 	},
@@ -79,15 +84,32 @@ let ff = {
 		for (var i = 1; i <= 6; i++) {
 			var unl = fluc_save.energy >= ff.data.reqs[i]
 			el("ff_arc_"+i).className = unl ? "ff_btn" : "unavailablebtn"
-			//el("ff_arc_"+i+"title").textContent = unl ? "Position" + i : "Locked"
-			//el("ff_arc_"+i+"cost").textContent = unl ? "(Cost: 1 FE)" : "(requires " + ff.data.reqs[i] + " FE)"
+			el("ff_arc_"+i+"_title").textContent = unl ? "Position " + i : "Locked"
+			el("ff_arc_"+i+"_cost").textContent = unl ? "(Cost: 1 FE)" : "(requires " + ff.data.reqs[i] + " FE)"
 
 			var pk = ff_save.perks[i]
 			el("ff_perk_"+i).className = unl ? "ff_btn" : "unavailablebtn"
-			el("ff_perk_"+i+"_name").textContent = pk ? ff.data[pk].title : "None"
-			el("ff_perk_"+i+"_desc").textContent = pk ? ff.data[pk].desc : ""
+			el("ff_perk_"+i+"_name").textContent = pk ? ff.data[ff.data.all[pk]].title : "None"
+			el("ff_perk_"+i+"_desc").textContent = pk ? ff.data[ff.data.all[pk]].desc : ""
 		}
 	},
+
+	choose(x, mode) {
+		if (mode == "perk") {
+			var newV = (ff_save.perks[x] || 0) + 1
+			var name = ff.data.all[newV]
+			if (newV == ff.data.all.length || fluc_save.energy < ff.data[name].req) newV = 0
+			ff_save.perks[x] = newV
+
+			el("ff_perk_"+x+"_name").textContent = newV ? ff.data[name].title : "None"
+			el("ff_perk_"+x+"_desc").textContent = newV ? ff.data[name].desc : ""
+		}
+	},
+	switchMode(update) {
+		if (!tmp.ngp3) return
+		if (!update) ff_save.mode = (ff_save.mode + 1) % 2
+		getEl("ff_mode").textContent = "Mode: " + ff.data.modes[ff_save.mode]
+	}
 }
 let ff_tmp = {}
 let ff_save
