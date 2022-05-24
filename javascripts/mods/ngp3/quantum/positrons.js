33867ee6988faa81196655c260abfc802d2d9805
aarextiaokhiao <aarextiaokhiao@gmail.com>
2022-05-23 23:08:19 -0400

    Started implementing

--- javascripts/mods/ngp3/quantum/positrons.js
+++ javascripts/mods/ngp3/quantum/positrons.js
@@ -1,3 +1,13 @@
 let pos = {
-	unl: () => tmp.ngp3 && mTs.has("d7")
+	unl: () => tmp.ngp3 && mTs.has("d7"),
+	setup() {
+		return {
+			s: []
+		}
+	}
+
+	order: ["r1", "b1", "b2", "b3", "r2", "b4", "r3"]
+	boosts: {
+		
+	}
 }
