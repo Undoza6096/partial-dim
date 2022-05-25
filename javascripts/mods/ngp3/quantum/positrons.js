58946dd0ad057305c141a61361e2d8d0b111a32b
aarextiaokhiao <aarextiaokhiao@gmail.com>
2022-05-24 23:13:21 -0400

    Update positrons.js

--- javascripts/mods/ngp3/quantum/positrons.js
+++ javascripts/mods/ngp3/quantum/positrons.js
@@ -8,6 +8,25 @@ let pos = {
 
 	order: ["r1", "b1", "b2", "b3", "r2", "b4", "r3"]
 	boosts: {
-		
+		1: {
+			name: "???",
+			eff: () => new Decimal(1),
+			desc: (x) => shorten(x),
+		},
+		2: {
+			name: "???",
+			eff: () => new Decimal(1),
+			desc: (x) => shorten(x),
+		}
+	},
+	res: {
+		1: {
+			name: "Placeholder",
+			res: () => new Decimal(0),
+		},
+		2: {
+			name: "Placeholder",
+			res: () => new Decimal(0),
+		}
 	}
 }
